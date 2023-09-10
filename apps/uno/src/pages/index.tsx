8388f746319ae486f61a544db75bfbeab0011831
joshborseth <joshuaborseth@gmail.com>
2023-09-10 16:46:49 -0700

    add toast error if they didn't fill in a name and code

--- apps/uno/src/pages/index.tsx
+++ apps/uno/src/pages/index.tsx
@@ -3,12 +3,22 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
 const Home = () => {
+  const { toast } = useToast();
+
+  const [inputState, setInputState] = useState({
+    name: "",
+    code: "",
+  });
   return (
     <main className="min-h-screen flex flex-col justify-center items-center gap-10">
-      <h1 className="text-2xl font-bold">Lets get ready to rumble!</h1>
-      <ModeToggle />
+      <div className="flex-col gap-10 hidden lg:flex justify-center items-center">
+        <h1 className="text-2xl font-bold">Lets get ready to rumble!</h1>
+        <ModeToggle />
+      </div>
 
       <section className="grid grid-cols-1 lg:grid-cols-3">
         <Card>
@@ -30,10 +40,21 @@ const Home = () => {
           </CardHeader>
           <CardContent className="flex flex-col gap-2 items-start">
             <Label htmlFor="name">Player Name:</Label>
-            <Input id="name" />
+            <Input onChange={(e) => setInputState({ ...inputState, name: e.target.value })} id="name" />
             <Label htmlFor="code">Game Code:</Label>
-            <Input id="code" />
-            <Button>Select</Button>
+            <Input onChange={(e) => setInputState({ ...inputState, code: e.target.value })} id="code" />
+            <Button
+              onClick={() => {
+                if (!inputState.name || !inputState.code)
+                  toast({
+                    variant: "destructive",
+                    title: "You are a Moron!",
+                    description: "Hey Moron! You have to fill out a name and a code to play 🤦‍♂️",
+                  });
+              }}
+            >
+              Select
+            </Button>
           </CardContent>
         </Card>
       </section>
