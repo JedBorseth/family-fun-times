54348b1031cce24a51142e2dfafa9f360cc48358
joshborseth <joshuaborseth@gmail.com>
2023-09-17 13:24:58 -0700

    fix room code gen

--- apps/uno/src/server/routers/room.ts
+++ apps/uno/src/server/routers/room.ts
@@ -1,12 +1,12 @@
 import { eq } from "drizzle-orm";
 import { Room } from "../db/schema";
 import { procedure, router } from "../trpc";
-import { nanoid } from "nanoid";
+import { fourRandomLetters } from "../helpers/roomCodeGen";
+
 export const roomRouter = router({
   create: procedure.mutation(async ({ ctx }) => {
-    console.log(ctx);
     const generateUniqueCode = async (): Promise<string> => {
-      const code = nanoid(4);
+      const code = fourRandomLetters();
       const rooms = await ctx.db.select().from(Room).where(eq(Room.code, code));
       if (rooms.length > 0) {
         return await generateUniqueCode();
